d6fe26fe3ee4d7fa16be40a3dffe3227339924ae
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2023-12-20 13:33:23 +0200

    Complete Listing Page

--- client/src/Pages/Listing.jsx
+++ client/src/Pages/Listing.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation} from 'swiper/modules';
 import 'swiper/css/bundle';
-
+import { FaBath, FaBed, FaChair, FaLocationDot, FaP, FaRegCopy, FaSquareParking } from "react-icons/fa6";
 export default function Listing() {
     SwiperCore.use([Navigation])
     const [error, setError] = useState(false)
@@ -40,16 +40,57 @@ export default function Listing() {
         {error && <p className='text-center my-7 text-2xl text-red-700 font-semibold'>Something went Wrong!</p>}
         {listing && !loading && !error && (
             <>
-                <Swiper navigation>
+                <Swiper className='relative' navigation>
                     {listing.imageUrls.map((url)=> (
                         <SwiperSlide key={url}>
-                            <div className="h-[550px]" style={{background: `url(${url}) center no-repeat`, backgroundSize: 'cover'}}>
-
+                            <div className="h-[550px] " style={{background: `url(${url}) center no-repeat`, backgroundSize: 'cover'}}>
+                                <span className='absolute top-8 right-8 rounded-full bg-slate-600 cursor-pointer text-4xl p-4'>
+                                    <FaRegCopy color='white'/>
+                                </span>
                             </div>
                         
                         </SwiperSlide>
                     ) )}
                 </Swiper>
+
+                <div className="flex flex-col max-w-2xl mx-auto my-10 gap-3 p-3">
+                    <h1 className='text-2xl font-semibold '>{`${listing.name} - Price : $${listing.regularPrice}`} </h1>
+                    <p className='flex justify-start items-center gap-4 text-xl mt-6'>
+                        <FaLocationDot color='green'  /> {`${listing.address}`} 
+                    </p>
+                    <div className="flex gap-2">
+                    <p className="bg-red-900 text-white w-full max-w-[200px] text-center p-1 rounded-md">
+                        {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                    </p>
+                        {listing.offer && (
+                            <p className="bg-green-900 text-white w-full max-w-[200px] text-center p-1 rounded-md">
+                                ${+listing.regularPrice - +listing.discountPrice} 
+                            </p>
+                        )}
+                    </div>
+                    <p className='mt-2'>
+                        <span className='text-slate-600 font-semibold text-xl'>Description - </span>{`${listing.description}`}
+                    </p>
+                    <ul className='flex gap-3 text-green-900 font-semibold text-sm mt-2 flex-wrap'>
+                        <li className='flex items-center whitespace-nowrap gap-2'>
+                            <FaBed className='text-lg gap-1' />
+                            {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : '1 Bed'}
+                        </li>
+                        <li className='flex items-center whitespace-nowrap gap-2'>
+                            <FaBath />
+                            {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : '1 Bath'}
+                        </li>
+                        <li className='flex items-center whitespace-nowrap gap-2'>
+                            <FaSquareParking />
+                            {listing.parking ? 'Yes Parking' : 'No Parking'}
+                        </li>
+                        <li className='flex items-center whitespace-nowrap gap-2'>
+                            <FaChair />
+                            {listing.furnished ? 'Yes Furnished' : 'Not Furnished'}
+                        </li>
+                    </ul>
+                    <Link to="" className="bg-slate-700 text-white text-center font-semibold my-3 p-3 rounded-lg w-full hover:opacity-95 disabled:opacity-80">Contact LandLord</Link>
+                </div>
             </>
         )}
     </main>

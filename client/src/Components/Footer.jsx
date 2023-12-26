c96f0a53fa68dde0df8a027458eb47683edbaf4b
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2023-12-26 18:25:33 +0200

    add Current Year to Footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Footer() {
+  const currentYear = (currentDate) => currentDate = new Date().getFullYear();
   return (
-    <div className='relative bottom-0 p-6 bg-slate-600 w-full mx-auto text-center text-white'>Made By <Link className='text-yellow-500' to='https://ibrahimhamdy.net/'>Ibrahim Hamdy</Link> &copy; 2024</div>
+    <div className='relative bottom-0 p-6 bg-slate-600 w-full mx-auto text-center text-white'>Made By <Link  className='text-yellow-500' to='https://ibrahimhamdy.net/'>Ibrahim Hamdy</Link> &copy; <span id='fullYear'>{currentYear()}</span></div>
   )
 }
